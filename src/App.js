c46605ccaf458ebdebf4caf3c076e55d6f3a18b1
akshay3851 <48242605+akshay3851@users.noreply.github.com>
2021-05-03 14:22:28 +0530

    "initial commit"

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import AddTodo from './components/AddTodo'
+import TodoList from './components/TodoList'
+import VisibilityFilters from './components/VisibilityFilters'
+import './App.css';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <h1>Todo List</h1>
+      <AddTodo />
+      <TodoList />
+      <VisibilityFilters />
+    </div>
+  );
+}
+
+export default App;
